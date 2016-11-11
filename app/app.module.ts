f66e335d4d58ec7d267f6e8f9c21f6e11067a7a3
stevenu8 <steven.h.u@outlook.com>
2016-11-10 22:28:02 -0800

    moving heroes to its own component

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms'
+
 import { AppComponent }   from './app.component';
+import { HeroesComponent }     from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
+
+import { HeroService } from './hero.service'
+
 @NgModule({
   imports: [ 
     BrowserModule, 
@@ -10,8 +15,10 @@ import { HeroDetailComponent } from './hero-detail.component';
   ],
   declarations: [ 
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent
   ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers: [HeroService ]
 })
 export class AppModule { }
