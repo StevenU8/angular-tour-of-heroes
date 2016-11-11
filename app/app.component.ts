41d4f3bebeaaf62d9a24162646ff3c7b335f5339
stevenu8 <steven.h.u@outlook.com>
2016-11-11 00:31:36 -0800

    pulling out css and html into seperate files

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,16 +1,12 @@
 import { Component } from '@angular/core';
-@Component({
-  selector: 'my-app',
-  template: `   
-   <h1>{{title}}</h1>
-   <nav>
-     <a routerLink="/dashboard">Dashboard</a>
-     <a routerLink="/heroes">Heroes</a>
-   </nav>
-   <router-outlet></router-outlet>
- `
 
+@Component({
+  moduleId: module.id,
+  selector: 'my-app',
+  templateUrl: 'app.component.html',
+  styleUrls: ['/app.  component.css']
 })
+
 export class AppComponent {
   title = 'Tour of Heroes';
 }
