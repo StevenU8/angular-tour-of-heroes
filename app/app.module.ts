46a0a49b64c0498b34255210b289117b76ec1cb8
stevenu8 <steven.h.u@outlook.com>
2016-11-22 13:26:31 -0800

    Learning about http module

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,23 +1,37 @@
+import './rxjs-extensions';
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+
+
+import { AppRoutingModule }     from './app-routing.module';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './heroes/in-memory-data.service';
+
 import { AppComponent }         from './app.component';
 import { DashboardComponent }   from './heroes/dashboard.component';
 import { HeroDetailComponent }  from './heroes/hero-detail.component';
 import { HeroesComponent }      from './heroes/heroes.component';
 import { HeroService }          from './heroes/hero.service';
-import { AppRoutingModule }     from './app-routing.module';
+import { HeroSearchComponent }  from './heroes/hero-search.component';
+
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   declarations: [
     AppComponent,
     DashboardComponent,
     HeroDetailComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroSearchComponent
   ],
   providers: [ HeroService ],
   bootstrap: [ AppComponent ]
